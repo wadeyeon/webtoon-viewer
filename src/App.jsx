dba52f267e92ecbabaaf5a40aca28c0d03d1eb7c
wadeyeon <wadeyeon@gmail.com>
2025-03-26 10:34:43 +0900

    Add: install material ui package and custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Outlet } from 'react-router';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f6d015',
+    },
+    secondary: {
+      main: '#153af6',
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Outlet />
-    </>
+    </ThemeProvider>
   );
 }
 
