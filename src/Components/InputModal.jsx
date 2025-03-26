8c605d24016bed0795493ae297f7193068c65839
wadeyeon <wadeyeon@gmail.com>
2025-03-26 13:44:27 +0900

    Add: InputModal component

---
+++ src/Components/InputModal.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+
+export default function InputModal({ open, handleClose }) {
+  const [column, setColumn] = useState('');
+
+  const handleChange = (e) => {
+    setColumn(e.target.value);
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      slotProps={{
+        paper: {
+          component: 'form',
+          onSubmit: (e) => {
+            e.preventDefault();
+            const formData = new FormData(e.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            const column = formJson.column;
+            console.log(column);
+
+            handleClose();
+          },
+        },
+      }}
+    >
+      <DialogContent>
+        <DialogContentText>열 갯수를 선택해주세요.</DialogContentText>
+        <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id='demo-simple-select-standard-label'>Column</InputLabel>
+          <Select
+            labelId='demo-simple-select-standard-label'
+            id='demo-simple-select-standard'
+            name='column'
+            value={column}
+            onChange={handleChange}
+          >
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+          </Select>
+        </FormControl>
+      </DialogContent>
+      <DialogActions>
+        <Button type='submit'>OK</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import styles from '../styles/Home.module.css';
+import InputModal from '../Components/InputModal';
 
 function Home() {
+  const [open, setOpen] = useState(false);
+
   return (
     <section className={styles.home}>
       <div className={styles.home__container}>
@@ -10,9 +14,10 @@ function Home() {
           <br />
           VIEWER
         </h1>
-        <Button variant='contained' size='large'>
+        <Button variant='contained' size='large' onClick={() => setOpen(true)}>
           START
         </Button>
+        <InputModal open={open} handleClose={() => setOpen(false)} />
       </div>
     </section>
   );
